2393772c8e9b631d7eee51e3140e743b4e969c3c
nokkvi96 <nokkvi96@gmail.com>
2021-10-21 20:40:34 +0000

    styled-system init

---
+++ src/theme/colors.ts
@@ -0,0 +1,16 @@
+export const colors = {
+  bg: "#FFF7F8",
+  primary: "#3A86FF",
+  error: "#F71735",
+  black: "#1c1c20",
+  white: "#fff",
+  succes: "#00A676",
+  danger: "#F71735",
+  neutral: "#8EE3F5",
+  grey: {
+    dark: "#1c1c20",
+    medium: "#c0c8da",
+    light: "#d8deec",
+    lighter: "#F2F2F2",
+  },
+};
