8f37e16ffc6f8d52b0fbcbec0f1a9d2d9b1bebda
Nökkvi <nokkvi96@gmail.com>
2021-10-28 15:43:54 +0000

    .babelrc created, eslint change, next-seo added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,57 @@
 module.exports = {
-  env: {
-    browser: true,
-    node: true,
-    es2020: true,
-  },
   parser: "@typescript-eslint/parser",
+  plugins: ["react-hooks", "simple-import-sort"],
+  extends: [
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "eslint-config-prettier/@typescript-eslint",
+  ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2018,
     sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
   },
-  plugins: ["@typescript-eslint", "react", "prettier"],
-  extends: [
-    "airbnb",
-    "airbnb/hooks",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "prettier",
-    "prettier/@typescript-eslint",
-    "prettier/react",
-  ],
   rules: {
-    "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }],
-    "import/extensions": "off",
-    "react/prop-types": "off",
-    "jsx-a11y/anchor-is-valid": "off",
-    "react/jsx-props-no-spreading": ["error", { custom: "ignore" }],
-    "prettier/prettier": "error",
-    "react/no-unescaped-entities": "off",
-    "import/no-cycle": [0, { ignoreExternal: true }],
-    "prefer-const": "off",
-    // needed because of https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md#how-to-use & https://stackoverflow.com/questions/63818415/react-was-used-before-it-was-defined
-    "no-use-before-define": "off",
-    "react-hooks/rules-of-hooks": "error",
-    "@typescript-eslint/no-use-before-define": [
-      "error",
-      { functions: false, classes: false, variables: true },
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+      },
     ],
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/ban-ts-ignore": "off",
+    "prettier/prettier": "off",
+    "simple-import-sort/sort": "warn",
+    "import/no-named-as-default-member": "off",
+    "import/default": "off",
+    // 'import/named': 'off',
+    // 'import/namespace': 'off',
+    "jsx-a11y/click-events-have-key-events": "off",
+    "jsx-a11y/anchor-is-valid": "off",
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/display-name": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
   },
   settings: {
+    react: {
+      version: "detect",
+    },
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
     "import/resolver": {
-      "babel-module": {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-      },
-      node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-        paths: ["src"],
+      typescript: {
+        alwaysTryTypes: true,
       },
     },
   },
