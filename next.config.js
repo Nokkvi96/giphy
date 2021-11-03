ba19a8175f59032d13ff7e070c5b1f1e86790ce7
Nökkvi <nokkvi96@gmail.com>
2021-11-03 21:01:48 +0000

    dotenv

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
+const Dotenv = require("dotenv-webpack");
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Add the new plugin to the existing webpack plugins
+    config.plugins.push(new Dotenv({ silent: true }));
+
+    return config;
+  },
+  env: {
+    AUTH_SECRET: process.env.API_KEY,
+  },
 };
