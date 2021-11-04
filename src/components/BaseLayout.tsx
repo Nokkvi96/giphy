bc5524e40387bc053671af2ec867a2aa1ca8a096
Nökkvi <nokkvi96@gmail.com>
2021-11-04 13:41:59 +0000

    bg = black

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -3,7 +3,7 @@ import { Box, Contain, Flex } from "@components/system";
 export const BaseLayout: React.FC = ({ children }) => {
   return (
     <Flex flexDirection="column" height="100vh">
-      <Box as="main" flex={"1 0 auto"} display="block" bg="bg">
+      <Box as="main" flex={"1 0 auto"} display="block" bg="black">
         <Contain>{children}</Contain>
       </Box>
     </Flex>
